c7a1e365a294b3e674b1d49971ef6b2f07038b5b
toriking <54404273+toriking@users.noreply.github.com>
2020-10-30 23:20:11 +0900

    Add files via upload

--- js/createText.js
+++ js/createText.js
@@ -1,2 +1,7 @@
-const createText = document.getElementById('text');
-createText.textContent = 'rr';
+const createText = document.getElementById('create-text');
+const input = document.getElementById('enter')
+createText.classList.add('show')
+const text ='abcdefghijklmnpoqrstuvvwxyzABCDEFGHIJKLMNPOQRSTUVVWXYZ0123456789';
+const jatext = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん';
+
+createText.textContent = Math.floor(Math.random * 8)
