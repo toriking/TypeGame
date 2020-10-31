3487a00de923c9bd3653458418a7f577858c3a6f
tomoki <toriiirito@outlook.jp>
2020-10-31 16:23:40 +0900

    a

--- js/createText.js
+++ js/createText.js
@@ -3,5 +3,3 @@ const input = document.getElementById('enter')
 createText.classList.add('show')
 const text ='abcdefghijklmnpoqrstuvvwxyzABCDEFGHIJKLMNPOQRSTUVVWXYZ0123456789';
 const jatext = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん';
-
-createText.textContent = Math.floor(Math.random * 8)
