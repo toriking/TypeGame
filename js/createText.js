bdbeed4630e1c4d2d900f8028f73e36c974cf557
toriking <54404273+toriking@users.noreply.github.com>
2020-11-02 20:21:13 +0900

    Add files via upload

--- js/createText.js
+++ js/createText.js
@@ -1,32 +1,52 @@
 const createText = document.getElementById('create-text');
 const enter = document.getElementById('enter');
 const retext = document.getElementById('retext');
-// const text = document.getElementById('text');
+const text = document.getElementById('text');
 const back = document.querySelector('.back')
 createText.classList.add('show');
 const entext ='abcdefghijklmnpoqrstuvvwxyzABCDEFGHIJKLMNPOQRSTUVVWXYZ0123456789';
 const jatext = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん';
 
 
-let ccc = createText.textContent = Math.random().toString(32).substring(2)
+// let ccc = createText.textContent = Math.random().toString(32).substring(2)
 
+// 生成する文字列の長さ
+var l = 8;
+
+// 生成する文字列に含める文字セット
+var c = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+var cl = c.length;
+create()
+function create(){
+    createText.textContent =''  
+    for(var i=0; i<l; i++){
+        createText.textContent += c[Math.floor(Math.random()*cl)];
+    }
+}
 function check(){
-    if(createText.textContent == text.textContent){
+    
+    if(createText.textContent == text.value){
         console.log('uu');
+        text.value = '';
         create();
         count();
     } else{
         console.log('yy');
     }
 }
-let aa= 0, bb= 15;
+
 const counttext = document.createElement('div');
 counttext.classList.add('count')
-counttext.textContent = aa + '/' + bb;
 back.appendChild(counttext);
 
-
-
+let score= 0, quizset = 15;
+counttext.textContent =  score  + '/' + quizset ;
+function count(){
+    
+    counttext.textContent = 1 + score  + '/' + quizset;
+     score++
+}
 
 retext.addEventListener('click', ()=>{
     create()
---
+++ js/test.js
@@ -0,0 +1,11 @@
+const scoreboard = document.getElementById('scoreboard');
+const countBtn = document.getElementById('countBtn')
+let score= 0, quizset = 15;
+scoreboard.textContent =  score  + '/' + quizset ;
+function count(){   
+    scoreboard.textContent = 1 + score  + '/' + quizset;
+    score++
+}
+countBtn.addEventListener('click', ()=>{
+    count()
+})
